34b83b95c258574c1ec684f3f28ff8f275de4fd4
Ian Kincaid <i.s.kincaid@gmail.com>
2024-04-05 17:29:50 -0400

    Initial Commit

---
+++ Exercise Calorie Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise Calorie Counter
+//
+//  Created by Ian Kincaid on 4/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
